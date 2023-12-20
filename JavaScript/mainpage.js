35f1f818035f77d5b259362eef6b7bee32e18ff5
MontyMango <cameronharter50@hotmail.com>
2023-12-20 12:56:25 -0500

    Some cool elements and fixes + Learned some javascript and applied the alert() to the main index + I guess button is a thing in html + On hover stuff has been added too to the main index page. = Fixed style.css, I forgot about a semicolon at the end of a statement ( :| whoops )

---
+++ JavaScript/mainpage.js
@@ -0,0 +1,11 @@
+function redButtonDecoy() {
+    alert('Thank you for not clicking the real red button. I urge you NOT to click the red button! Please!')
+}
+
+function actualRedButton()  {
+    alert('What have you done 0_0')
+}
+
+function greenButton()  {
+    alert('Oh, thank you for complying and clicking the green button. Enjoy the portfolio website!')
+}
